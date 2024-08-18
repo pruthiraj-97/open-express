26d64daf97a4c12061b608794941537f9aa4e383
pruthiraj-97 <b122085@iiit-bh.ac.in>
2024-08-19 00:16:08 +0530

    server-bug-resolve

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ const express=require('express')
 const app=express()
 const PORT=4000
 
-app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
+function startServer(){
+    try {
+        app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
+    } catch (error) {
+        console.log("error in server connection")
+        process.env(1)
+    }
+}
+
+startServer()
